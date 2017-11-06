770292223307afbfb4305a19947df43dd6628794
mewa <marcin.k.chmiel@gmail.com>
2017-11-06 02:38:54 +0100

    Add basic ESAC->MIDI conversion

--- src/Esac.hs
+++ src/Esac.hs
@@ -6,13 +6,45 @@ module Esac
 import Control.Monad
 import Data.Char
 import Data.Ratio
-import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec hiding (State) 
+import Codec.Midi
+import Control.Monad.State.Lazy
 
 data MidiNote = MidiNote {
   octave :: Int
   , pitch :: Int
   , duration :: Float
   } deriving (Show)
+
+data MidiState = MidiState {
+  now :: Ticks
+  , baseDuration :: Ticks
+  , basePitch :: Key
+  } deriving (Show)
+
+makeTrack :: Ticks -> [MidiNote] -> Track Ticks
+makeTrack base notes = join $ evalState (mapM makeNote notes) (MidiState 0 base 40)
+
+makeNote :: MidiNote -> State MidiState (Track Ticks)
+makeNote note = do
+  s <- get
+  let dur = round $ (fromIntegral $ baseDuration s) * duration note
+  return $ [(0, NoteOn 0 (pitch note + basePitch s) 40)
+           , (dur, NoteOff 0 (pitch note + basePitch s) 40)]
+
+midi :: [MidiNote] -> Tempo -> Int -> Midi
+midi notes tempo shortest = let
+  ticksPerShortest = round $ 96 / (fromIntegral shortest / 4)
+  in Midi SingleTrack (TicksPerBeat 96)
+     [makeTempo tempo : makeTrack ticksPerShortest notes]
+
+
+makeTempo tempo = (0, TempoChange (floor $ 1000000.0 / (fromIntegral tempo / 60.0)))
+
+melodyToMidi melody = case parseEsac melody of
+                        Left e -> print e
+                        Right notes -> do
+                          exportFile "xa.mid" $ midi notes 90 8
 
 parseEsac :: String -> Either ParseError [MidiNote]
 parseEsac = parse parseMelody "(Not a valid EsAC)"
