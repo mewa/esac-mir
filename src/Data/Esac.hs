c210f4aaba97a87df25d9c4585e4d88a679b1992
mewa <marcin.k.chmiel@gmail.com>
2017-11-26 04:42:51 +0100

    Added explicit intervals handling in ESAC

--- src/Data/Esac.hs
+++ src/Data/Esac.hs
@@ -36,21 +36,60 @@ data EsacKey = EsacKey {
 
 data EsacNote = EsacNote {
   octave :: Int
-  , num :: Int -- num is 0 indexed (as opposed to 1 in the textual notation)
+  , interval :: Interval
   , sharpness :: PitchMod
   , duration :: Float
-  } deriving (Show)
+  } deriving (Show, Eq)
+
+data Interval = Interval Int
+  deriving (Show, Eq)
 
 data PitchMod = Sharp | Flat | None
+  deriving (Eq)
 
 instance Show PitchMod where
   show Sharp = "#"
   show Flat = "b"
   show _ = ""
 
+instance Enum PitchMod where
+  fromEnum Sharp = 1
+  fromEnum Flat = -1
+  fromEnum _ = 0
+  toEnum 1 = Sharp
+  toEnum (-1) = Flat
+  toEnum _ = None
+
 data Note = Note {
   noteDiv :: Int
   } deriving (Show)
 
-data Sound = C | CSharp | D | DSharp | E | F | FSharp | G | GSharp | A | ASharp | B
-  deriving (Enum, Show)
+data BaseSound = C | D | E | F | G | A | B
+  deriving (Show, Eq)
+
+data Sound = Sound BaseSound PitchMod
+  deriving (Show, Eq)
+
+cdur :: [BaseSound]
+cdur = let
+  interval = [C, D, E, F, G, A, B]
+  in interval ++ cdur
+
+
+makeInterval :: BaseSound -> [BaseSound]
+makeInterval bs = take 7 . dropWhile (bs /=) $ cdur
+
+addInterval :: Sound -> Interval -> Sound
+addInterval s@(Sound bs m) (Interval interval) = let
+  customInterval = makeInterval bs
+  in Sound (customInterval !! (interval - 1)) m
+
+intervalDisplacement :: [BaseSound] -> Sound -> Int
+intervalDisplacement interval to@(Sound base mod) = let
+  disp = foldl (\val n -> val + intervalNoteValue n) 0 $ takeWhile (base /=) $ interval
+  in disp + fromEnum mod
+
+intervalNoteValue :: BaseSound -> Int
+intervalNoteValue E = 1
+intervalNoteValue _ = 2
+
