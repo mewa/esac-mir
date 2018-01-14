cd47b258af93a50746d72a2f8ef58b0ab0a7f3c9
mewa <marcin.k.chmiel@gmail.com>
2018-01-14 01:56:30 +0100

    Added base64 MIDI response

--- src/Server.hs
+++ src/Server.hs
@@ -9,27 +9,34 @@ import Data.Esac.Parser
 import Data.Esac.Converter
 import Data.Either
 import Data.Midi.Parser
+import Data.Monoid
 import Control.Monad.IO.Class
 import Web.Scotty
 import Control.Monad.Reader
 import qualified Data.Text.Lazy as TL
-import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.ByteString.Lazy.Char8 as LC
+import qualified Data.ByteString as BS
 import Codec.Midi
 import Network.HTTP.Types.Status
 import Network.Wai.Parse
 import Debug.Trace
+import qualified Data.ByteString.Base64.URL.Lazy as Base64
+import qualified Data.Map as Map
 
 serve :: Int -> IO ()
 serve port = scotty port $ do
   post "/esac2midi" $ do
     tempo <- rescue (param "tempo") (const $ return 90) :: ActionM Int
     octave <- rescue (param "octave") (const $ return 4) :: ActionM Int
+    format <- rescue (param "format") (const $ return "") :: ActionM String
     esac <- jsonData :: ActionM EsacJson
     liftIO $ print esac
     case esacFromJson esac of
       Right m -> do
-        setHeader "Content-Type" "audio/midi"
-        raw . midiBytes . runReader (midiFromEsac tempo octave) $ m
+        let midi = midiBytes . runReader (midiFromEsac tempo octave) $ m
+        case format of
+          "file" -> midiFileResponse midi
+          _ -> base64MidiResponse midi
       Left e -> do
         status badRequest400
         text . TL.pack . show $ e
@@ -44,3 +51,13 @@ serve port = scotty port $ do
         status badRequest400
         text . TL.pack . show $ e
       Right mel -> json mel
+
+base64midi = mappend "data:audio/midi;base64," . Base64.encode
+
+base64MidiResponse midi = do
+  setHeader "Content-Type" "application/json"
+  json $ Map.fromList [("midi64" :: String, LC.unpack $ base64midi midi)]
+
+midiFileResponse midi = do
+  setHeader "Content-Type" "audio/midi"
+  raw midi
