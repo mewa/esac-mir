0fe3e2a92ec6c06a0b457eae20bddd8d83264e38
mewa <marcin.k.chmiel@gmail.com>
2018-01-19 01:17:53 +0100

    Relaxed DB action function's IO requirements to MonadIO

--- src/Storage/Db.hs
+++ src/Storage/Db.hs
@@ -7,10 +7,13 @@ import qualified Data.Map.Strict as Map
 import Database.MongoDB
 import Data.Bson
 import qualified Data.Aeson as Aeson
-import qualified Data.Text as TL
+import qualified Data.Text as T
 import Data.Char
+import Control.Monad.IO.Class
 
 run pipe action = access pipe master "esacdb" action
+
+connect = Database.MongoDB.connect . host
 
 collection_esacs = "esacs"
 
@@ -20,7 +23,7 @@ field_melodyRhythm = "melody_rhythm"
 
 esacBson :: EsacJson -> Document
 esacBson esac = let
-  toMap = Aeson.fromJSON . Aeson.toJSON :: EsacJson -> Aeson.Result (Map.Map TL.Text TL.Text)
+  toMap = Aeson.fromJSON . Aeson.toJSON :: EsacJson -> Aeson.Result (Map.Map T.Text T.Text)
   toDocument k v l = (k =: v) : l
   (Aeson.Success result) = Map.foldrWithKey toDocument [] <$> toMap esac
   in result
@@ -36,26 +39,40 @@ computedEsacBson esac = let
   melody_rhythm = filter (not . isPitchMod) . fmap eraseNote . melody $ esac
   in [field_melodyRaw =: melody_raw, field_melodyRhythm =: melody_rhythm] ++ esacBson esac
 
-getEsacs :: Action IO [Document]
-getEsacs = find (select [] collection_esacs) >>= rest
+esacJson :: Document -> EsacJson
+esacJson doc = defaultEsacJson {
+  name = at "name" doc
+  , title = at "title" doc
+  , source = at "source" doc
+  , region = at "region" doc
+  , signature = at "signature" doc
+  , key = at "key" doc
+  , melody = at "melody" doc
+  , remarks = at "remarks" doc
+  }
 
-getEsac :: TL.Text -> Action IO (Maybe Document)
-getEsac id = findOne (select ["_id" =: id] collection_esacs)
+jsonAction d = fmap (fmap esacJson) d
 
-addEsac :: EsacJson -> Action IO Value
+getEsacs :: (MonadIO m) => Action m [EsacJson]
+getEsacs = jsonAction $ find (select [] collection_esacs) >>= rest
+
+getEsac :: (MonadIO m) => ObjectId -> Action m (Maybe EsacJson)
+getEsac id = jsonAction $ findOne (select ["_id" =: id] collection_esacs)
+
+addEsac :: (MonadIO m) => EsacJson -> Action m Value
 addEsac esac = insert collection_esacs $ computedEsacBson esac
 
-removeEsac :: TL.Text -> Action IO ()
+removeEsac :: (MonadIO m) => ObjectId -> Action m ()
 removeEsac id = delete $ select ["_id" =: id] collection_esacs
 
-updateEsac :: TL.Text -> EsacJson -> Action IO ()
+updateEsac :: (MonadIO m) => ObjectId -> EsacJson -> Action m ()
 updateEsac id esac = modify (select ["_id" =: id] collection_esacs) $ computedEsacBson esac
 
-findMelody :: TL.Text -> Action IO [Document]
+findMelody :: (MonadIO m) => T.Text -> Action m [Document]
 findMelody pattern = rest =<< find (select [field_melody =: Regex pattern ""] collection_esacs)
 
-findMelodyRaw :: TL.Text -> Action IO [Document]
+findMelodyRaw :: (MonadIO m) => T.Text -> Action m [Document]
 findMelodyRaw pattern = rest =<< find (select [field_melodyRaw =: Regex pattern ""] collection_esacs)
 
-findMelodyRhythm :: TL.Text -> Action IO [Document]
+findMelodyRhythm :: (MonadIO m) => T.Text -> Action m [Document]
 findMelodyRhythm pattern = rest =<< find (select [field_melodyRhythm =: Regex pattern "*m*"] collection_esacs)
